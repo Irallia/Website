0ae57c05b63492bf255b5a834e6d3a61e765e5c8
MIRACULIX\Irallia <irallia@chrigelyra.de>
2018-05-21 01:44:57 +0200

    multiple images

--- src/app/BlogEntry.ts
+++ src/app/BlogEntry.ts
@@ -3,5 +3,5 @@ export class BlogEntry {
     name: string;
     text: string;
     author: string;
-    imagePath: string;
+    imagePaths: string[];
   }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,9 +9,9 @@ export class DataService {
   private blogEntries = new BehaviorSubject<BlogEntry[]>([
     { id: 1, name: 'Erster Eintrag: Abschied...',
       text: 'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ', 
-      author: 'Lydia', imagePath:'/images/MonkeyBotton.jpg'},
-    { id: 2, name: 'Kurz vor dem Abflug', text: 'blubb', author: 'Lydia', imagePath:''},
-    { id: 3, name: '48 Stunden oder doch nur 38 Stunden?', text: 'blubb', author: 'Stefan', imagePath:''}
+      author: 'Lydia', imagePaths: ['assets/images/photo_2015-12-12.jpg', 'assets/images/photo_2015-12-12.jpg']},
+    { id: 2, name: 'Kurz vor dem Abflug', text: 'blubb', author: 'Lydia', imagePaths:[]},
+    { id: 3, name: '48 Stunden oder doch nur 38 Stunden?', text: 'blubb', author: 'Stefan', imagePaths:[]}
   ]);
   blogEntry = this.blogEntries.asObservable();
 
